2c9bbe13396e7f13fe95a6e51662f17adae66bcc
DerrickPikachu <derrfgh4563@gmail.com>
2022-02-27 16:42:56 +0800

    Setup testing file

---
+++ src/VehicleTest.java
@@ -0,0 +1,36 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testFinalize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSpeed() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setDir() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSpeed() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDir() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void totalVehicle() {
+    }
+}
